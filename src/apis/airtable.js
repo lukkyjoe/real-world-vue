457e92358e7c0cc326f1369599684e413162cfa6
Joseph Luk <joseph.sc.luk@gmail.com>
2019-10-08 23:39:00 -0700

    Add airtable api.

---
+++ src/apis/airtable.js
@@ -0,0 +1,34 @@
+var Airtable = require("airtable");
+var AirtableKey = require("../env.json");
+var base = new Airtable({ apiKey: AirtableKey.AIRTABLE_KEY }).base(
+  AirtableKey.AIRTABLE_BASE
+);
+
+export const getIndustries = () => {
+  return new Promise((resolve, reject) => {
+    base("industries")
+      .select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 8,
+        view: "Grid view"
+      })
+      .firstPage(function page(error, records) {
+        if (error) reject(error);
+        // This function (`page`) will get called for each page of records.
+        let resultsArray = [];
+        records.forEach(function(record) {
+          let recordName = record.get("Name");
+          console.log("Retrieved", recordName);
+          if (recordName) {
+            resultsArray.push(record.get("Name"));
+          }
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        console.log("api records", records);
+        resolve(resultsArray);
+      });
+  });
+};
