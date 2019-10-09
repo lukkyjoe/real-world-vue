1c873bc2768619b43c63a574ccee3e870df3ee51
Joseph Luk <joseph.sc.luk@gmail.com>
2019-10-08 23:39:12 -0700

    Add async select component.

--- src/views/Select.vue
+++ src/views/Select.vue
@@ -1,26 +1,48 @@
 <!-- Vue component -->
 <template>
   <div>
-    <multiselect v-model="value" :options="options"></multiselect>
+    <label class="typo__label" for="ajax">Async multiselect</label>
+    <multiselect v-model="selectedCountries" id="ajax" label="name" track-by="code" placeholder="Type to search" open-direction="bottom" :options="countries" :multiple="true" :searchable="true" :loading="isLoading" :internal-search="false" :clear-on-select="false" :close-on-select="false" :options-limit="300" :limit="3" :limit-text="limitText" :max-height="600" :show-no-results="false" :hide-selected="true" @search-change="asyncFind">
+      <template slot="tag" slot-scope="{ option, remove }"><span class="custom__tag"><span>{{ option.name }}</span><span class="custom__remove" @click="remove(option)">❌</span></span></template>
+      <template slot="clear" slot-scope="props">
+        <div class="multiselect__clear" v-if="selectedCountries.length" @mousedown.prevent.stop="clearAll(props.search)"></div>
+      </template><span slot="noResult">Oops! No elements found. Consider changing the search query.</span>
+    </multiselect>
+    <pre class="language-json"><code>{{ selectedCountries  }}</code></pre>
   </div>
 </template>
 
 <script>
-  import Multiselect from 'vue-multiselect'
+import Multiselect from 'vue-multiselect'
+import { getIndustries } from '../apis/airtable'
 
-  // register globally
-//   Vue.component('multiselect', Multiselect)
-
-  export default {
-    // OR register locally
-    components: { Multiselect },
-    data () {
-      return {
-        value: null,
-        options: ['list', 'of', 'options']
-      }
+export default {
+  components: {
+    Multiselect
+  },
+  data () {
+    return {
+      selectedCountries: [],
+      countries: [],
+      isLoading: false
+    }
+  },
+  methods: {
+    limitText (count) {
+      return `and ${count} other countries`
+    },
+    asyncFind (query) {
+      this.isLoading = true
+      getIndustries(query).then(response => {
+        this.countries = response
+        this.isLoading = false
+      })
+    },
+    clearAll () {
+      this.selectedCountries = []
     }
   }
+}
 </script>
 
 <!-- New step!
