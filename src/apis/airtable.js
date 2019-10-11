36612e898e172dab8f77eacdd13326070eb6bd1d
Joseph Luk <joseph.sc.luk@gmail.com>
2019-10-11 16:14:24 -0700

    Airtable get results.

--- src/apis/airtable.js
+++ src/apis/airtable.js
@@ -32,3 +32,33 @@ export const getIndustries = () => {
       });
   });
 };
+
+export const getResults = filterValue => {
+  return new Promise((resolve, reject) => {
+    base("business")
+      .select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 8,
+        view: "Grid view",
+        filterByFormula: `{Name}=${filterValue}`
+      })
+      .firstPage(function page(error, records) {
+        if (error) reject(error);
+        // This function (`page`) will get called for each page of records.
+        let resultsArray = [];
+        records.forEach(function(record) {
+          let recordName = record.get("Name");
+          console.log("Retrieved", recordName);
+          if (recordName) {
+            resultsArray.push(record.get("Name"));
+          }
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        console.log("api records", records);
+        resolve(resultsArray);
+      });
+  });
+};
