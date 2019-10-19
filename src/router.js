3a3cd22aa81403b3e0b0af15070194c94af668ec
Joseph Luk <joseph.sc.luk@gmail.com>
2019-10-19 07:49:56 -0700

    Reroute things and make search center.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Search from "./views/ModelAjax.vue"
 
 Vue.use(Router);
 
@@ -11,7 +12,7 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Search
     },
     {
       path: "/about",
