4c4d0dc9aa618dcffbfde16204d68733dca30fbd
Joseph Luk <joseph.sc.luk@gmail.com>
2020-03-01 16:39:25 -0800

    Add 404 for bad paths.

--- src/router.js
+++ src/router.js
@@ -37,6 +37,10 @@ export default new Router({
       path: '/autosuggest',
       name: "autosuggest",
       component: () => import("./views/Autosuggest.vue")
+    },
+    {
+      path: '/*',
+      component: 404
     }
   ]
 });
